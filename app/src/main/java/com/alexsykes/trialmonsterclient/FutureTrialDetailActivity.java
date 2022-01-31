d8442abec916ecb5711c9a121b548346de309223
Alex Sykes <alex@alexsykes.net>
2022-01-31 11:18:35 +0000

    Adding event details - WiP

--- app/src/main/java/com/alexsykes/trialmonsterclient/FutureTrialDetailActivity.java
+++ app/src/main/java/com/alexsykes/trialmonsterclient/FutureTrialDetailActivity.java
@@ -1,12 +1,18 @@
 package com.alexsykes.trialmonsterclient;
 
 import android.os.Bundle;
-import android.text.Html;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -36,44 +42,46 @@ public class FutureTrialDetailActivity extends AppCompatActivity
         introTextView = findViewById(R.id.introTextView);
         followingTextView = findViewById(R.id.followingTextView);
 
-        introTextView.setText("intro");
-        introTextView.setText(Html.fromHtml("        <h1>Main Title</h1>\n" +
-                "        <h2>A sub-title</h2>\n" +
-                "        <p>This is some html. Look, here\\'s an <u>underline</u>.</p>\n" +
-                "        <p>Look, this is <em>emphasized.</em> And here\\'s some <b>bold</b>.</p>\n" +
-                "        <p>This is a UL list:\n" +
-                "        <ul>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ul>\n" +
-                "        <p>This is an OL list:\n" +
-                "        <ol>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ol>"));
-        followingTextView.setText(Html.fromHtml("        <h1>Main Title</h1>\n" +
-                "        <h2>A sub-title</h2>\n" +
-                "        <p>This is some html. Look, here\\'s an <u>underline</u>.</p>\n" +
-                "        <p>Look, this is <em>emphasized.</em> And here\\'s some <b>bold</b>.</p>\n" +
-                "        <p>This is a UL list:\n" +
-                "        <ul>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ul>\n" +
-                "        <p>This is an OL list:\n" +
-                "        <ol>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ol>"));
+        getTrialDetails(trialid);
 
         // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void getTrialDetails(String trialid) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getApplication().getBaseContext());
+        String url = "https://android.trialmonster.uk/getFutureTrialDetailAndroid.php?id=" + trialid;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("Info", "Response: " + response);
+//                        try {
+//                            theTrialList = getTrialList(response);
+//                            rv = findViewById(R.id.rv);
+//                            llm = new LinearLayoutManager(rv.getContext());
+//                            rv.setLayoutManager(llm);
+//                            rv.setHasFixedSize(true);
+//                            initialiseAdapter();
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Info", "That didn't work!");
+
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     @Override
